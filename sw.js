28c7e01381570e021607a409b53add9c04bde630
bappewgw-byte <bappewgw@gmail.com>
2026-07-02 03:29:16 +0700

    Remove console log and add skipWaiting in install

--- sw.js
+++ sw.js
@@ -1,16 +1,14 @@
 self.addEventListener('install', event => {
-    console.log('Service Worker: Installed');
+    self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
     console.log('Service Worker: Activated');
 });
 
-// Menangani ketika notifikasi diklik oleh user di HP
 self.addEventListener('notificationclick', event => {
     event.notification.close();
     event.waitUntil(
-        clients.openWindow('/') // Buka aplikasi pas notifikasi diklik
+        clients.openWindow('/')
     );
 });
-
